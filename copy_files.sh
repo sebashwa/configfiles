68a319716e765b9a0041e365ba7e8cabe40e8afb
Sebastian Warmbrunn <sebastianwarmbrunn@gmail.com>
2014-09-26 08:34:28 +0200

    add tmux config file & ability to copy paste from tmux to macosx

--- copy_files.sh
+++ copy_files.sh
@@ -1,6 +1,8 @@
-echo 'copying .zshrc to ~/.zshrc'
 cp .zshrc ~/.zshrc
-echo 'copying .vimrc to ~/.vimrc'
+echo 'copied .zshrc to ~/.zshrc'
 cp .vimrc ~/.vimrc
-echo 'copying .gitconfig to ~/.gitconfig'
+echo 'copied .vimrc to ~/.vimrc'
 cp .gitconfig  ~/.gitconfig
+echo 'copied .gitconfig to ~/.gitconfig'
+cp .tmux.conf  ~/.tmux.conf
+echo 'copied .tmux.conf to ~/.tmux.conf'

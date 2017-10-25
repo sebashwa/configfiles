b8fe2850463f4f92725efc1dd175b6949190fda9
Sebastian Warmbrunn <sebastianwarmbrunn@gmail.com>
2017-10-25 19:36:16 +0200

    Update xmonad.hs

--- xmonad.hs
+++ xmonad.hs
@@ -1,26 +1,27 @@
 import XMonad
-import XMonad.Config.Desktop
 import XMonad.Layout.NoBorders
 import XMonad.Layout.StackTile
 import XMonad.Layout.TwoPane
+import XMonad.Prompt.Pass
 import qualified XMonad.StackSet as W
 import XMonad.Util.EZConfig(additionalKeys)
 
 main = do
-    xmonad $ desktopConfig
+    xmonad $ defaultConfig
         { terminal           = myTerminal
         , normalBorderColor  = myNormalBorderColor
         , focusedBorderColor = myFocusedBorderColor
         , manageHook         = myManageHook
         , layoutHook         = myLayout
+        , modMask            = mod4Mask
         } `additionalKeys`
-        [ ((mod1Mask .|. shiftMask, xK_p), spawn "passmenu")
-        , ((mod1Mask .|. shiftMask, xK_l), spawn "slock & systemctl suspend")
+        [ ((mod4Mask .|. shiftMask, xK_p), spawn "passmenu")
+        , ((mod4Mask .|. shiftMask, xK_l), spawn "slock & systemctl suspend")
         , ((0, 0x1008FF02), spawn "light -A 20")
         , ((0, 0x1008FF03), spawn "light -U 20")
         , ((0, 0x1008FF11), spawn "amixer set Master 2-")
         , ((0, 0x1008FF13), spawn "amixer set Master 2+")
-        , ((0, 0x1008FF12), spawn "amixer set Master toggle")
+        , ((0, 0x1008FF12), spawn "pactl set-sink-mute 0 toggle")
         , ((0, 0x1008FF14), spawn "playerctl play-pause")
         , ((0, 0x1008FF16), spawn "playerctl prev")
         , ((0, 0x1008FF17), spawn "playerctl next")
@@ -30,9 +31,9 @@ myTerminal = "st"
 myNormalBorderColor = "#666666"
 myFocusedBorderColor = "#FFFFFF"
 
-myManageHook = composeAll [ resource =? "chromium"         --> doF (W.shift "2")
+myManageHook = composeAll [ resource =? "firefox"          --> doF (W.shift "2")
                           , resource =? "slack"            --> doF (W.shift "3")
                           , resource =? "telegram-desktop" --> doF (W.shift "3")
-                          ] <+> manageHook desktopConfig
+                          ] <+> manageHook defaultConfig
 
 myLayout = noBorders (Full) ||| StackTile 1 (3/100) (4/6) ||| TwoPane (3/100) (1/2)
